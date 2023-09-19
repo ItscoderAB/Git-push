7643520e625059f6cb93712e335d76e824d224f3
itscoderAB <abhimathur0406@gmail.com>
2023-09-19 14:28:39 +0530

    first git push

---
+++ index.js
@@ -0,0 +1,14 @@
+import http from "http";
+import fs from "fs";
+const port = process.env.PORT || 3000;
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    res.statusCode = 404;
+    res.setHeader('Content-type','text/html');
+    res.end('<h1> tihs is lorem</h1> <p>he how r you? </p>');
+
+})
+
+server.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+});
